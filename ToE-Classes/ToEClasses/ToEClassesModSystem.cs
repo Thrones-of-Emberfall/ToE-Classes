a0b51b13d46a3daabef2093c5a5bb72e7141a1d3
Zaedin <brunoasnunes98@gmail.com>
2026-04-22 17:31:38 +0100

    Add harmony patch to lock GrindingWheelPatches.cs to smiths only

--- ToE-Classes/ToEClasses/ToEClassesModSystem.cs
+++ ToE-Classes/ToEClasses/ToEClassesModSystem.cs
@@ -1,4 +1,6 @@
-﻿using Vintagestory.API.Client;
+﻿using System;
+using HarmonyLib;
+using Vintagestory.API.Client;
 using Vintagestory.API.Server;
 using Vintagestory.API.Config;
 using Vintagestory.API.Common;
@@ -7,16 +9,30 @@ namespace ToEClasses;
 
 public class ToEClassesModSystem : ModSystem
 {
+    private Harmony harmony;
+    
     // Called on server and client
     // Useful for registering block/entity classes on both sides
     public override void Start(ICoreAPI api)
     {
         api.RegisterBlockClass("AnvilLocked", typeof(AnvilLocked));
         api.RegisterBlockClass("BellowsLocked", typeof(BellowLocked));
+        // api.RegisterBlockClass("GrindingWheelLocked", typeof(GrindingWheelLocked));
         api.RegisterBlockClass("HelveLocked", typeof(HelveLocked));
         api.RegisterBlockClass("PulverizerLocked", typeof(PulverizerLocked));
         api.RegisterItemClass("RollerLocked", typeof(RollerLocked));
         api.RegisterBlockClass("BoilerLocked", typeof(BoilerLocked));
+        
+        try
+        {
+            harmony = new Harmony(Mod.Info.ModID);
+            harmony.PatchAll();
+            api.World.Logger.Notification($"{Mod.Info.Name}: Harmony patches enabled.");
+        }
+        catch (Exception ex)
+        {
+            api.World.Logger.Error($"{Mod.Info.Name}: Exception during Harmony patching: {ex}");
+        }
     }
 
     public override void StartServerSide(ICoreServerAPI api)
