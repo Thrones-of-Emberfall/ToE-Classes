94ef217e4233f666c2695c0215b4c2f39c65ed6a
Zaedin <brunoasnunes98@gmail.com>
2026-07-10 11:22:58 +0100

    Add dynamic mod patch helper Lock herbalist pot to apothecary

--- ToE-Classes/ToEClasses/ToEClassesModSystem.cs
+++ ToE-Classes/ToEClasses/ToEClassesModSystem.cs
@@ -1,4 +1,8 @@
-﻿using HarmonyLib;
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using ToEClasses.Patches;
 using Vintagestory.API.Common;
 
 namespace ToEClasses;
@@ -12,6 +16,56 @@ public class ToEClassesModSystem : ModSystem
         if (Harmony.HasAnyPatches(Mod.Info.ModID)) return;
         _harmony = new Harmony(Mod.Info.ModID);
         _harmony.PatchAll();
+        
+        if (api.ModLoader.IsModEnabled("herbalistpotsfork"))
+        {
+            DynamicPatchMod(api,
+                "HerbPots",
+                "BlockHerbalistPot",
+                "OnBlockInteractStart",
+                BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic,
+                typeof(BlockHerbalistPotPatch).GetMethod(nameof(BlockHerbalistPotPatch.OnBlockInteractStartPrefix)));
+        }
+
         api.World.Logger.Notification($"{Mod.Info.Name}: Harmony patches enabled.");
+    }
+    
+    private void DynamicPatchMod(ICoreAPI api,
+        string assemblyName,
+        string targetTypeName,
+        string originalMethodName,
+        BindingFlags originalMethodFlags,
+        MethodInfo? prefixMethod = null,
+        MethodInfo? postfixMethod = null)
+    {
+        var herbAsm = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.GetName().Name == assemblyName);
+
+        if (herbAsm == null)
+        {
+            api.Logger.Warning($"[{_harmony.Id}] {assemblyName} not loaded. Skipping patch.");
+            return;
+        }
+
+        var targetType = herbAsm.GetTypes().FirstOrDefault(t => t.Name == targetTypeName);
+
+        if (targetType == null)
+        {
+            api.Logger.Warning($"[{_harmony.Id}] {assemblyName} is enabled, but type '{targetTypeName}' could not be located.");
+            return;
+        }
+
+        var originalMethod = targetType.GetMethod(originalMethodName, originalMethodFlags);
+
+        if (originalMethod == null)
+        {
+            api.Logger.Error($"[{_harmony.Id}] Could not find method '{originalMethodName}' on type '{targetTypeName}'.");
+            return;
+        }
+
+        _harmony.Patch(
+            originalMethod,
+            prefix: prefixMethod != null ? new HarmonyMethod(prefixMethod) : null,
+            postfix: postfixMethod != null ? new HarmonyMethod(postfixMethod) : null);
+        api.Logger.Warning($"[{_harmony.Id}] Successfully injected Harmony patch into {assemblyName}!");
     }
 }
